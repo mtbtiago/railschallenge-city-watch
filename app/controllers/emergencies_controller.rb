a9465fd6c3280174e6b16006854e224fb77b6bac
mtbtiago <tiago.ameller@gmail.com>
2015-04-19 13:36:43 +0200

    WIP. more testing, add pry and other debugging helper tools

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -4,11 +4,23 @@ class EmergenciesController < ApplicationController
   end
 
   def create
-    render_fail_response
+    emergency = Emergency.create(
+      params.require(:emergency).permit(:code, :fire_severity, :police_severity, :medical_severity))
+
+    result = {:emergency => {}}
+    result[:emergency][:code] = emergency.code
+    result[:emergency][:fire_severity] = emergency.fire_severity
+    result[:emergency][:police_severity] = emergency.police_severity
+    result[:emergency][:medical_severity] = emergency.medical_severity
+
+    render json: result, status: 201
   end
 
   def show
-    emergency = Emergency.find_by(code: params[:name])
+    # puts "kkkkk #{params[:id]}"
+
+
+    emergency = Emergency.find_by(code: params[:id])
 
     result = {:emergency => {}}
     result[:emergency][:code] = emergency.code
